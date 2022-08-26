c64b5111a27d98f0051d78c7b20655e146c801fd
balbinfurio <xxbalbin@gmail.com>
2022-08-26 16:35:38 -0500

    assertions test

---
+++ DEMOS/src/04-assertions.test.js
@@ -0,0 +1,32 @@
+// matchers
+test('test object', () => {
+  const data = { name: 'balbin' };
+  data.lastName = 'furio';
+  data.age = 21;
+  expect(data).toEqual({ name: 'balbin', lastName: 'furio', age: 21 }); // When we're working with objects we use ".toEqual"
+});
+
+test('null', () => {
+  const data = null;
+  expect(data).toBeNull();
+  expect(data).toBeDefined();
+  expect(data).not.toBeUndefined();
+});
+
+test('booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+
+test('string', () => {
+  expect('epic').toMatch(/pic/);
+});
+
+test('list / arrays', () => {
+  const numbers = [1, 2, 3, 4, 5];
+  expect(numbers).toContain(5);
+});
